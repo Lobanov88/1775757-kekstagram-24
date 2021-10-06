4b41341e3218f7e432b6db5c355279e6e42f580b
Lobanov88 <lobanov.ilya.1988@gmail.com>
2021-10-06 18:16:44 +0300

    Модуляция

---
+++ js/get-random-number.js
@@ -0,0 +1,13 @@
+const getRandomNumber = (min, max) => {
+  // https://learn.javascript.ru/task/random-int-min-max
+  if (max < min) {
+    console.log('Неправильно задан интервал. Первое число должно быть меньше.');
+    return;
+  }
+  const rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+};
+
+getRandomNumber(5, 75);
+
+export {getRandomNumber};
