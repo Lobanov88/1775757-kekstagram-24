d256c5bf7e38d9e48b59f02f469fa503c8888c82
Lobanov88 <lobanov.ilya.1988@gmail.com>
2021-10-28 14:53:20 +0300

    Правда или действие (часть 1)

--- js/create-miniature.js
+++ js/create-miniature.js
@@ -1,12 +1,13 @@
 const createMiniature = (photos) => {
   const pictureTemplate = document.querySelector('#picture').content;
 
-  const template = pictureTemplate.querySelector('a');
+  // const template = pictureTemplate.querySelector('a');
 
   const photosFragment = document.createDocumentFragment();
 
   photos.forEach((photo) => {
-    const photoElement = template.cloneNode(true);
+    // const photoElement = template.cloneNode(true);
+    const photoElement = pictureTemplate.cloneNode(true);
     photoElement.querySelector('.picture__img').src = photo.url;
     photoElement.querySelector('.picture__likes').textContent = photo.likes;
     photoElement.querySelector('.picture__comments').textContent = photo.comments.length;
