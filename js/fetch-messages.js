7a81c76a33436efcbb6ec742e09b84b07e5a1495
Lobanov88 <lobanov.ilya.1988@gmail.com>
2021-11-12 08:59:54 +0300

    Надо подкачаться

--- js/fetch-messages.js
+++ js/fetch-messages.js
@@ -5,6 +5,8 @@ const body = document.querySelector('body');
 const errorContainer = document.querySelector('#error').content;
 const errorTemplate = errorContainer.querySelector('section');
 const errorMessage = errorTemplate.cloneNode(true);
+body.appendChild(errorMessage);
+errorMessage.classList.add('hidden');
 
 const closeErrorMessage = () => {
   errorMessage.classList.add('hidden');
@@ -20,7 +22,7 @@ function onErrorMessageEscKeydown(evt) {
 }
 
 const showErrorMessage = () => {
-  body.appendChild(errorMessage);
+  errorMessage.classList.remove('hidden');
 
   document.addEventListener('keydown', onFetchMessageEscKeydown);
 };
@@ -28,7 +30,10 @@ const showErrorMessage = () => {
 const successContainer = document.querySelector('#success').content;
 const successTemplate = successContainer.querySelector('section');
 const successMessage = successTemplate.cloneNode(true);
-const closeSuccessButton = successContainer.querySelector('.success__button');
+body.appendChild(successMessage);
+successMessage.classList.add('hidden');
+
+const closeSuccessButton = document.querySelector('.success__button');
 
 const closeSuccessMessage = () => {
   successMessage.classList.add('hidden');
@@ -46,7 +51,7 @@ function onFetchMessageEscKeydown(evt) {
 }
 
 const showSuccessMessage = () => {
-  body.appendChild(successMessage);
+  successMessage.classList.remove('hidden');
 
   document.addEventListener('keydown', onFetchMessageEscKeydown);
 };
